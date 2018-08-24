cdc1cd6c38fa9c49fb7dd7d567fdb2964bd3ec8e
Lauri Kyttälä <lauri.kyttala@gmail.com>
2018-08-24 14:06:21 +0300

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -9,27 +9,35 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-  // console.log(req.body);
   var todo = new Todo({
     text: req.body.text
   });
   todo.save().then((doc) => {
     res.send(doc);
-  }, (err) => {
-    res.status(400).send(err);
+  }, (e) => {
+    res.status(400).send(e);
   });
 });
 
-app.post('/users', (req, res) => {
-  var user = new User({
-    email: req.body.email
-  });
-  user.save().then((doc) => {
-    res.send(doc);
-  }, (err) => {
-    res.status(400).send(err);
-  });
-});
+
+// app.get('/todos', (req, res) => {
+//   Todo.find().then((todos) => {
+//     res.send({todos});
+//   }, (e) => {
+//     res.status(400).send(e);
+//   });
+// });
+
+// app.post('/users', (req, res) => {
+//   var user = new User({
+//     email: req.body.email
+//   });
+//   user.save().then((doc) => {
+//     res.send(doc);
+//   }, (e) => {
+//     res.status(400).send(e);
+//   });
+// });
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
