8f78a37ff9e743955c7e56ed0f1a44c6ff11627b
Lauri Kyttälä <lauri.kyttala@gmail.com>
2018-08-31 11:54:35 +0300

    Hash user password before save

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc';
 
-// https://jwt.io/   to check
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded: ', decoded)
+var hashedPassword = '$2a$10$E8OXwJQmGeaqij8SEq6R/ujW6JI3dHFA8S3TiEj6ayla5b2P4XYqu';
+
+bcrypt.compare('123!', hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+// var data = {
+//   id: 10
+// };
+//
+// // https://jwt.io/   to check
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded: ', decoded)
 
 
 
