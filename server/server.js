813da3438711224f88b40a55d7eddbb9d16c455d
Lauri Kyttälä <lauri.kyttala@gmail.com>
2018-09-01 14:48:01 +0300

    Added tests for POST /users/login

--- server/server.js
+++ server/server.js
@@ -115,8 +115,6 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
-// POST /users/login {email, password}
-
 app.post('/users/login', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
 
@@ -128,17 +126,6 @@ app.post('/users/login', (req, res) => {
     res.status(400).send();
   });
 });
-
-// app.post('/users/login', (req, res) => {
-//   var body = _.pick(req.body, ['email', 'password']);
-//
-//     User.findByCredentials(body.email, body.password).then((user) => {
-//       res.send(user);
-//       // res.sendStatus(user);
-//     }).catch((e) => {
-//       res.status(400).send();
-//     });
-// });
 
 
 app.listen(port, () => {
